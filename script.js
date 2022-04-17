5ecc89fe80f72a93b067af1f93150c7ecf1c644d
SarahRex <sarahrexmakes@gmail.com>
2022-04-17 17:37:45 -0400

    commit message

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+console.log(
+  "I didnt write a poem for six years because I was afraid of what would happen once I turned the rusty faucet on. I was afraid I would lose myself. "
+);
+
+console.log(
+  "I was afraid I had forgotten how. More than anything, I think, I was afraid to be bad at something. "
+);
+var countDownDate = new Date("May 17, 2022 12:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
